87c0a76795da5f0e35764576ead5e1e3fcffa447
dandyidk <45296953+dandyidk@users.noreply.github.com>
2024-09-02 19:43:53 +0300

    Update TIMER.c

--- testing.X/TIMER.c
+++ testing.X/TIMER.c
@@ -1,8 +1,8 @@
 #include <avr/io.h>
 #include "TIMER.h"
 void TIMER_INIT() {
-    DDRD |=(1<<DDD6);
+    
     TCCR0A |= (1<<COM0A1) | (1<<WGM01) |(1<<WGM00);
     TCCR0B |= (1<<CS00) | (1<<CS01);
 }
-void ti_run(int duty_cycle){OCR0A =(duty_cycle*255)/1023;}
+void ti_run(int duty_cycle){OCR0A =(uint8_t)(duty_cycle/4);}
